4e1367d8f9861c135f058d1c9ae796998e169251
New-AH <thomas.george@edu.ece.fr>
2019-11-18 19:11:17 +0100

    Ajax

--- 2-express.js
+++ 2-express.js
@@ -1,4 +1,5 @@
 express = require('express')
+metrics = require('./metrics')
 app = express()
 
 path = require('path')
@@ -12,6 +13,13 @@ app.get(
   '/hello/:name', 
   (req, res) => res.render('hello.ejs', {name: req.params.name})
 )
+
+app.get('/metrics.json', (req, res) => {
+  metrics.get((err, data) => {
+    if(err) throw err
+    res.status(200).json(data)
+  })
+})
 
 app.listen(
   app.get('port'), 
